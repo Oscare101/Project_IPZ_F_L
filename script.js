b3eb55d114a1481a5e55c18ff4c53771d99a955b
Oscare101 <54881555+Oscare101@users.noreply.github.com>
2020-05-17 11:47:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var arr_touches = [];
+var canvas;
+var ctx;
+var down = false; 
+var color = 'black'; 
+let width = 5; 
+
 window.onload = function() {
     //handling mouse click and move events
     canvas.addEventListener('mousemove', handleMove); 
@@ -11,3 +18,160 @@ window.onload = function() {
     canvas.addEventListener("touchleave", handleEnd, false);
     canvas.addEventListener("touchmove", handleTouchMove, false);
 };
+
+function fun1() {
+	var rng=document.getElementById('r1'); //rng - это ползунок
+	var i1=document.getElementById('i1'); // i1 - input
+	i1.value=rng.value;
+	var width = Number(i1.value);
+}
+function handleMove(e)
+{
+	xPos = e.clientX-canvas.offsetLeft;
+	yPos = e.clientY-canvas.offsetTop;
+	if(down == true)
+	{
+		ctx.lineTo(xPos,yPos); 
+		ctx.strokeStyle = color;
+		ctx.stroke();
+	}
+}
+function handleDown() 
+{
+    down = true;
+    ctx.beginPath();
+    ctx.moveTo(xPos, yPos);
+}
+function handleUp() 
+{
+    down = false;
+}
+function handleStart(evt) 
+{
+    var touches = evt.changedTouches;
+    for(var i = 0; i < touches.length; i++) 
+    {
+	    if(isValidTouch(touches[i])) 
+	    {
+	        evt.preventDefault();
+	        arr_touches.push(copyTouch(touches[i]));
+	        ctx.beginPath();
+	        ctx.fillStyle = color;
+	        ctx.fill();
+        }
+    }
+}
+function handleTouchMove(evt) 
+{
+    var touches = evt.changedTouches;
+    var offset = findPos(canvas);
+    for (var i = 0; i < touches.length; i++) 
+    {
+		if(isValidTouch(touches[i])) 
+		{
+	        evt.preventDefault();
+	        var idx = ongoingTouchIndexById(touches[i].identifier);
+    	    if (idx >= 0) 
+    	    {
+    	        ctx.beginPath();
+    	        ctx.moveTo(arr_touches[idx].clientX-offset.x, arr_touches[idx].clientY-offset.y);
+    	        ctx.lineTo(touches[i].clientX-offset.x, touches[i].clientY-offset.y);
+    	        ctx.strokeStyle = color;
+    	        ctx.stroke();
+    	        
+    	        arr_touches.splice(idx, 1, copyTouch(touches[i]));
+    	    }   
+	    }
+    }
+}
+function handleEnd(evt) 
+{
+    var touches = evt.changedTouches;
+    var offset = findPos(canvas);
+    for (var i = 0; i < touches.length; i++) 
+    {
+		if(isValidTouch(touches[i])) 
+		{
+		    evt.preventDefault();
+	        var idx = ongoingTouchIndexById(touches[i].identifier);
+	        if (idx >= 0) 
+	        {
+	            ctx.lineWidth = 4;
+	            ctx.fillStyle = color;
+	            ctx.beginPath();
+	            ctx.moveTo(arr_touches[idx].clientX-offset.x, arr_touches[idx].clientY-offset.y);
+	            ctx.lineTo(touches[i].clientX-offset.x, touches[i].clientY-offset.y);
+	           arr_touches.splice(i, 1);
+	        } 
+        }
+	}
+}
+function handleCancel(evt) 
+{
+    evt.preventDefault();
+    var touches = evt.changedTouches;
+  
+    for (var i = 0; i < touches.length; i++) {
+	    arr_touches.splice(i, 1);
+    }
+}
+function copyTouch(touch) 
+{
+    return {identifier: touch.identifier,clientX: touch.clientX,clientY: touch.clientY};
+}
+function ongoingTouchIndexById(idToFind) 
+{
+    for (var i = 0; i < arr_touches.length; i++) {
+	    var id = arr_touches[i].identifier;
+	    if (id == idToFind) {
+	        return i;
+	    }
+    }
+    return -1;
+}
+function changeColor(new_color) 
+{
+    color = new_color;
+}
+function clearCanvas() 
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function isValidTouch(touch) 
+{
+    var curleft = 0, curtop = 0;
+    var offset = 0;
+    
+	if (canvas.offsetParent) {
+		do {
+			curleft += canvas.offsetLeft;
+			curtop += canvas.offsetTop;
+		} while (touch == canvas.offsetParent);
+	    
+	    offset = { x: curleft-document.body.scrollLeft, y: curtop-document.body.scrollTop };
+	}
+    
+    if(touch.clientX-offset.x > 0 &&
+	        touch.clientX-offset.x < parseFloat(canvas.width) &&
+	            touch.clientY-offset.y >0 &&
+	                touch.clientY-offset.y < parseFloat(canvas.height)) {
+        return true;
+    }
+	else 
+	{
+	    return false;
+	}
+}
+function findPos(obj) 
+{
+	var curleft = 0, curtop = 0;
+	if (obj.offsetParent) 
+	{
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj == obj.offsetParent);
+
+		return { x: curleft-document.body.scrollLeft, y: curtop-document.body.scrollTop };
+	}
+}
